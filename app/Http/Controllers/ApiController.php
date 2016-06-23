ff922eac443b2098287eac57976c544b5e78e322
sergofan1@yandex.ru <sergofan1@yandex.ru>
2016-06-23 17:58:48 +0300

    Update controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,10 +14,17 @@ class ApiController extends Controller
         $data['rows'] = (!empty($request->input('rows'))) ? $request->input('rows') : '0';
         $data['timeframe'] = (!empty($request->input('timeframe'))) ? $request->input('timeframe') : '5';
 
+        $operation = 'Null';
+        $direction = 'Null';
+        $counter = 0;
+        $open = 'Null';
+        $close = 'Null';
+        $min = 'Null';
+        $max = 'Null';
+        $allVolume = 0;
+
         $maxRows = app('db')->table('st_' . $data['currency'])->where([['created_at', '>=', $data['bar']], ['tVolume', '>=', $data['volume']]])->get();
-        $counter = 1;
         $counterTemp = 1;
-        $direction = '';
         for ($i = 0; $i < count($maxRows); $i++) {
             if ($i !== 0) {
                 if ($maxRows[$i]->tDirection == $maxRows[$i - 1]->tDirection) {
@@ -32,9 +39,35 @@ class ApiController extends Controller
             }
         }
 
-        if (empty($direction)) $direction = 'Null';
-        return $data;
-        //return $this->response();
+        if (!empty($direction) and $counter >= $data['rows']){
+            $bar = strtotime(date('Y-m-d H:i:00', time()) . "- " . $data['timeframe'] . " min");
+            $rows = app('db')->table('st_' . $data['currency'])
+                ->select('tPrice', 'tVolume', 'tDirection')
+                ->where('created_at', '>', $bar)
+                ->orderBy('id', 'asc')
+                ->get();
+            if (!empty($rows)) {
+                $arraySize = count($rows) - 1;
+                $max = max($rows)->tPrice;
+                $min = min($rows)->tPrice;
+                $open = $rows[0]->tPrice;
+                $close = $rows[$arraySize]->tPrice;
+                $rows = $rows;
+                foreach ($rows as $row) {
+                    $allVolume += $row->tVolume;
+                }
+
+                if ($direction == 'Buy' and $open > $close and $close <= $min) {
+                    $operation = 'Sell';
+                } elseif ($direction == 'Sell' and $open < $close and $close >= $max) {
+                    $operation = 'Buy';
+                }else{
+                    $operation = 'Null';
+                }
+
+            }
+        }
+        return $this->response($operation,$direction,$counter,$open,$close,$min,$max);
     }
 
     private function response($operation = 'Null', $direction = 'Null', $counter = 'Null', $open = 'Null', $close = 'Null', $min = 'Null', $max = 'Null')
